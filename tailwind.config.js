203cded592e2d0cf3d139ef80418c5a8f689327a
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-09-15 18:52:38 +0600

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
       },
       backgroundColor:{
         "softRed":"rgba(251,96,135,0.1)",
+        "softDarkBlue":"rgba(182,189,200,0.9)",
+        "softBlack":"rgba(0,0,0,0.5)",
+        "softVeryBlue":"rgba(29,32,37,0.3)"
+      },
+      spacing:{
+        "box":"150px",
       }
     },
   },
