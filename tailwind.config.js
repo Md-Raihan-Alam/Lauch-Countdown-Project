0b19b7cf60ec128e61c8788ad97aef60eb3cbfa8
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-09-17 19:22:19 +0600

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,28 @@ module.exports = {
         "hills":"images/pattern-hills.svg",
       },
       backgroundColor:{
-        "softRed":"rgba(251,96,135,0.1)",
-        "softDarkBlue":"rgba(182,189,200,0.9)",
-        "softBlack":"rgba(0,0,0,0.5)",
-        "softVeryBlue":"rgba(29,32,37,0.3)"
+        "white":"hsl(0, 0%, 100%)",
+        "darkDesaturatedBlue":"hsl(236, 21%, 26%)",
+        "veryDarkBlue":"hsl(235, 16%, 14%)",
+        "verDark":"hsl(234, 17%, 12%)",
+        "grayishBlue":"hsl(237, 18%, 59%)",
+        "softRed":"hsl(345, 95%, 68%)",
+        "softRedBg":"rgba(251,96,135,0.2)",
+        "grayishBLueBg":"rgba(132,134,169,0.2)",
+        "darkDesaturatedBlueBg":"rgba(52,54,80,0.2)",
+        "veryDarkBlueBg":"rgba(30,31,41,0.2)",
+        "veryDarkBlue":"rgba(25,26,36,0.7)"
+      },
+      colors:{
+        "darkDesaturatedBlue":"hsl(236, 21%, 26%)",
+        "veryDarkBlue":"hsl(235, 16%, 14%)",
+        "verDark":"hsl(234, 17%, 12%)",
+        "grayishBlue":"hsl(237, 18%, 59%)",
+        "softRed":"hsl(345, 95%, 68%)",
       },
       spacing:{
         "box":"150px",
-      }
+      },
     },
   },
   plugins: [],
