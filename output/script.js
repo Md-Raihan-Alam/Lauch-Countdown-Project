2d195433a51c4f7c0a36f5d36cfae8bcd7e0efa6
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-09-21 21:04:56 +0600

    WIP

--- output/script.js
+++ output/script.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("working");
+let facebookIcon = document.querySelector('#facebook');
+let instragramIcon = document.querySelector('#instragram');
+let pinterestIcon = document.querySelector('#pinterest');
+function overColor(e) {
+    console.log("Working");
+    e.children[0].setAttribute("fill", "#fb6087");
+}
+function outColor(e) {
+    // console.log('working');
+    e.children[0].setAttribute("fill", "#8385A9");
+}
+facebookIcon.addEventListener('mouseover', function () { overColor(facebookIcon); });
+instragramIcon.addEventListener('mouseover', function () { overColor(instragramIcon); });
+pinterestIcon.addEventListener('mouseover', function () { overColor(pinterestIcon); });
+facebookIcon.addEventListener('mouseout', function () { outColor(facebookIcon); });
+instragramIcon.addEventListener('mouseout', function () { outColor(instragramIcon); });
+pinterestIcon.addEventListener('mouseout', function () { outColor(pinterestIcon); });
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,19 @@
-console.log("working");
+let facebookIcon=document.querySelector('#facebook') as HTMLElement;
+let instragramIcon=document.querySelector('#instragram') as HTMLElement;
+let pinterestIcon=document.querySelector('#pinterest') as HTMLElement;
+function overColor(e:HTMLElement)
+{
+    console.log("Working");
+    e.children[0].setAttribute("fill","#fb6087")
+}
+function outColor(e:HTMLElement)
+{
+    // console.log('working');
+    e.children[0].setAttribute("fill","#8385A9")
+}
+facebookIcon.addEventListener('mouseover',function(){overColor(facebookIcon);});
+instragramIcon.addEventListener('mouseover',function(){overColor(instragramIcon);});
+pinterestIcon.addEventListener('mouseover',function(){overColor(pinterestIcon);});
+facebookIcon.addEventListener('mouseout',function(){outColor(facebookIcon)});
+instragramIcon.addEventListener('mouseout',function(){outColor(instragramIcon)});
+pinterestIcon.addEventListener('mouseout',function(){outColor(pinterestIcon)});
